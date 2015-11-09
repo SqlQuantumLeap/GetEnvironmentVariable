90b77a45cbce6aeed9d545ee0734939db7bc9301
Sql Quantum Leap <SqlQuantumLeap@users.noreply.github.com>
2015-11-09 12:51:19 -0500

    Initial project setup

---
+++ GetEnvironmentVariable/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * "Get Environment Variable"
+ * Copyright (c) 2015 Sql Quantum Leap. All rights reserved.
+ * 
+ */
+using System;
+
+namespace GetEnvironmentVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ GetEnvironmentVariable/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * "Get Environment Variable"
+ * Copyright (c) 2015 Sql Quantum Leap. All rights reserved.
+ * 
+ */
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Get current value of User or System env variable")] // File description
+[assembly: AssemblyDescription("")] // ?
+[assembly: AssemblyConfiguration("")] // ?
+[assembly: AssemblyCompany("Sql Quantum Leap")]
+[assembly: AssemblyProduct("Get Environment Variable")] // Product name
+[assembly: AssemblyCopyright("Copyright © 2015 Sql Quantum Leap. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6a0f8484-f685-4a54-bc1d-d2a7058a8552")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")] // Product version
+[assembly: AssemblyFileVersion("1.0.0.0")] // File version
+
+
